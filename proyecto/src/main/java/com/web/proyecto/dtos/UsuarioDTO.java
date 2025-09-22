abe3152ba8cc5dc86de8bb7952bdc957216e5adb
Violeta <violeta.fajardo@javeriana.edu.co>
2025-09-21 21:37:49 -0500

    Agregué cambios de usuario

--- proyecto/src/main/java/com/web/proyecto/dtos/UsuarioDTO.java
+++ proyecto/src/main/java/com/web/proyecto/dtos/UsuarioDTO.java
@@ -1,5 +1,6 @@
 package com.web.proyecto.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
@@ -7,6 +8,7 @@ public class UsuarioDTO {
     private Long id;
     private String nombre;
     private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private Long empresaId; // referencia a la empresa
 }
