238589e42f3a94c680b1fa90d2502767e387c283
Juan Sebastian Vargas <151252035+Jusebbb@users.noreply.github.com>
2025-09-23 10:24:18 -0500

    Update ActivityRepository.java

--- proyecto/src/main/java/com/web/proyecto/repositories/ActivityRepository.java
+++ proyecto/src/main/java/com/web/proyecto/repositories/ActivityRepository.java
@@ -1,6 +1,7 @@
 package com.web.proyecto.repositories;
 
 import com.web.proyecto.entities.Activity;
+import com.web.proyecto.entities.Status;   // NUEVO
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,11 +12,14 @@ public interface ActivityRepository extends JpaRepository<Activity, Long> {
     boolean existsByNameIgnoreCaseAndProcess_Id(String name, Long processId);
     List<Activity> findByProcess_Id(Long processId);
 
-    // paralelos a Process.* (OJO: Process tiene empresaId, status, category)
+    // paralelos a Process
     List<Activity> findByProcess_EmpresaId(Long empresaId);
     List<Activity> findByProcess_Status(String status);
     List<Activity> findByProcess_Category(String category);
 
     // combinado
     List<Activity> findByProcess_EmpresaIdAndProcess_Status(Long empresaId, String status);
+
+    // NUEVO: listar todas menos las eliminadas
+    List<Activity> findByStatusNot(Status status);
 }
