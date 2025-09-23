607d8398f50290dcf4b88f3761d85f94218a2436
Juan Sebastian Vargas <151252035+Jusebbb@users.noreply.github.com>
2025-09-23 10:21:05 -0500

    Update ActivityDTO.java

--- proyecto/src/main/java/com/web/proyecto/dtos/ActivityDTO.java
+++ proyecto/src/main/java/com/web/proyecto/dtos/ActivityDTO.java
@@ -1,11 +1,14 @@
 package com.web.proyecto.dtos;
 
+import com.web.proyecto.entities.Status;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
-@Getter @Setter
-@NoArgsConstructor @AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ActivityDTO {
 
@@ -13,6 +16,13 @@ public class ActivityDTO {
 
     @NotBlank(message = "name es obligatorio")
     private String name;
+
+    // ===== HU-08 =====
+    private String type;
+    private String description;
+    private Long roleId;
+
+    private Status status; // se envía/recibe como ACTIVE/INACTIVE/DELETED
 
     @NotNull(message = "processId es obligatorio")
     private Long processId;
