8664409b1ba456d794ef7a0dd1de97f6f3eec6e7
mgcruzp <gcruzpino@gmail.com>
2025-10-20 18:36:12 -0500

    Creacion componente login

--- frontProyecto/src/app/app.routes.ts
+++ frontProyecto/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { Login } from './pages/login/login';
-import { Register } from './pages/register/register';
+import { LoginComponent } from './pages/login/login';
+import { RegisterComponent  } from './pages/register/register';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 
-  { path: 'login', component: Login },
-  { path: 'register', component: Register },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent  },
 
   { path: '**', redirectTo: 'login' },
 ];
--- frontProyecto/src/app/pages/login/login.ts
+++ frontProyecto/src/app/pages/login/login.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrls: ['./login.scss'],
 })
-export class Login {
+export class LoginComponent {
+  form!: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+    if (this.form.invalid) { this.form.markAllAsTouched(); return; }
+    console.log('LOGIN =>', this.form.value);
+  }
 }
