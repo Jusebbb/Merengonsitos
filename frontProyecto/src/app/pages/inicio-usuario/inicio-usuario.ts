3104c74a0fb79dd612767875ade80b526ba13afa
mgcruzp <gcruzpino@gmail.com>
2025-10-26 01:06:20 -0500

    error bobo

--- frontProyecto/src/app/pages/inicio-usuario/inicio-usuario.ts
+++ frontProyecto/src/app/pages/inicio-usuario/inicio-usuario.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-inicio-Usuario',
+  selector: 'app-inicio-usuario',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './inicio-Usuario.html',
-  styleUrls: ['./inicio-Usuario.scss'],
+  templateUrl: './inicio-usuario.html',   // <- en minúsculas
+  styleUrls: ['./inicio-usuario.scss'],   // <- en minúsculas
 })
 export class InicioUsuarioComponent {}
