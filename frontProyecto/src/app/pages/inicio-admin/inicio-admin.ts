27f0b0848f48f0f4a9a2010dcf33380610826b45
mgcruzp <gcruzpino@gmail.com>
2025-10-26 00:41:47 -0500

    Inicio usuario mas lindo

--- frontProyecto/src/app/pages/inicio-admin/inicio-admin.ts
+++ frontProyecto/src/app/pages/inicio-admin/inicio-admin.ts
@@ -1,18 +1,17 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';  // Asegúrate de importar Router
-import { RouterModule } from '@angular/router';  // Asegúrate de importar RouterModule
 
 @Component({
   selector: 'app-inicio-admin',
   standalone: true,  // Esto hace que el componente sea independiente
-  imports: [RouterModule],  // Asegúrate de importar RouterModule aquí
+  imports: [],  // Ya no necesitas importar Router aquí
   templateUrl: './inicio-admin.html',
   styleUrls: ['./inicio-admin.scss'],
 })
 export class InicioAdminComponent {
-  constructor(private router: Router) {}
+  //constructor(private router: Router) {}
 
-  goToCreateUser() {
-    this.router.navigate(['/crear-usuario']);  // Redirige a la página de creación de usuario
-  }
+  //goToCreateUser() {
+    //this.router.navigate(['/crear-usuario']);  // Usa la ruta absoluta
+  //}
 }
