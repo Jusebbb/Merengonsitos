64d7d3b2285696107e90a67375935e15d7883979
mgcruzp <gcruzpino@gmail.com>
2025-10-26 01:50:15 -0500

    Creacion de login DTO

---
+++ frontProyecto/src/app/dtos/loginDto.ts
@@ -0,0 +1,18 @@
+
+export type RolUsuario = 'ADMIN' | 'EDITOR' | 'LECTOR';
+
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+
+export interface LoginResponse {
+  token: string;
+
+  role?: RolUsuario | string;
+  rol?: RolUsuario | string;
+  userId?: string;
+  name?: string;
+}
